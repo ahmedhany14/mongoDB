552f2d0c488ef509e706ef5ace8f547a7d9d0028
Ahmed Hany <ah7608867@gmail.com>
2024-10-21 16:35:11 +0300

    User embedded docs

--- models/user.js
+++ models/user.js
@@ -4,7 +4,21 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
     },
-    age: Number
+    age: Number,
+    isInstactor: {
+        type: Boolean,
+        default: false
+    },
+    cources: [
+        {
+            titel: String,
+            describtion: String,
+            CreatedAt: {
+                type: Date,
+                default: Date.now
+            },
+        }
+    ]
 })
 const users = mongoose.model('users', userSchema)
 
